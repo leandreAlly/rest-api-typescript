6993bcc2df1a5ec9029e4376ae9bec3c1eb96c4d
allyleandre <tuyambazeleandre@gmail.com>
2023-07-11 13:17:54 +0200

    Build login endpoint

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -2,6 +2,37 @@ import { createUser, getUserByEmail } from "../db/users";
 import express from "express";
 import { authentication, random } from "../helpers";
 
+export const login = async (req: express.Request, res: express.Response) => {
+  const { email, password } = req.body;
+
+  const user = await getUserByEmail(email).select(
+    "+authentication.salt +authentication.password"
+  );
+
+  if (!user) {
+    return res.status(400).json({ message: "Invalid credintial" });
+  }
+
+  const expectedHash = authentication(user.authentication.salt, password);
+
+  if (user.authentication.password !== expectedHash) {
+    return res.status(403).json("Invalid credential");
+  }
+
+  const salt = random();
+
+  user.authentication.sessionToken = authentication(salt, user._id.toString());
+
+  await user.save();
+
+  res.cookie("ALLY-REST-API", user.authentication.sessionToken, {
+    domain: "localhost",
+    path: "/",
+  });
+
+  return res.status(200).json(user).end();
+};
+
 export const register = async (req: express.Request, res: express.Response) => {
   try {
     console.log("hit endpoint");
